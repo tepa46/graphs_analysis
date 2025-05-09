6dde73a2ff8148e03b149ad99cd1d795bef7ebdf
Kirill Shishin <shishin_ka@mail.ru>
2025-05-10 02:45:27 +0300

    feat: Implement SingleSourceParentBFS with graphBLAS

---
+++ parent_bfs/GraphBLAS/bench_runner.py
@@ -0,0 +1,6 @@
+from SingleSourceParentBFSAlgo import SingleSourceParentBFSAlgo
+from bench import run_bench
+
+if __name__ == "__main__":
+    algo = SingleSourceParentBFSAlgo()
+    run_bench(algo)

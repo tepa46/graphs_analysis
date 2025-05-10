aa30f3d25e3efab960d325ccbc222b280c472cda
Kirill Shishin <shishin_ka@mail.ru>
2025-05-10 18:09:35 +0300

    feat: Change Algo interface

--- algo.py
+++ algo.py
@@ -1,8 +1,13 @@
 class Algo:
     # Extracts data from dataset in the format required for run algorithm
+    #
+    # NOTE! Use received data as [data] argument in the run method.
     def load_data_from_dataset(self, dataset):
         pass
 
     # Runs algorithm
-    def run(self, data):
+    #
+    # [additional_data] is additional data needed to run the algorithm
+    # For example, start nodes (sources) for parent-BFS algorithms.
+    def run(self, data, additional_data=None):
         pass

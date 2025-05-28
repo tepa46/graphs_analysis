dff3af3499b29de157fafd77f613dbbc57a44cd7
LeonidElkin <113133848+LeonidElkin@users.noreply.github.com>
2025-05-28 15:41:35 +0300

    refactor: msbfs

--- src/algo/MSBFS/Gunrock/msbfs.hxx
+++ src/algo/MSBFS/Gunrock/msbfs.hxx
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <gunrock/algorithms/algorithms.hxx>
+#include <unordered_map>
 #include <vector>
 
 namespace gunrock {
@@ -36,22 +37,38 @@ namespace gunrock {
 			using edge_t = typename graph_t::edge_type;
 			using weight_t = typename graph_t::weight_type;
 
+			thrust::device_vector<vertex_t> batch_ids;
+			thrust::device_vector<vertex_t> needed_next;
+			thrust::device_vector<vertex_t> needed_next_size;
+
 			void init() override {
 			}
 
 			void reset() override {
 				auto num_vertices = this->get_graph().get_number_of_vertices();
 				auto batch_size = param.sources.size();
+				batch_ids.resize(num_vertices * batch_size);
+				needed_next.resize(num_vertices * batch_size);
+				needed_next_size.resize(num_vertices);
 
 				auto d_predecessors = thrust::device_pointer_cast(this->result.predecessors);
 				auto d_distances = thrust::device_pointer_cast(this->result.distances);
+				auto d_batches = thrust::device_pointer_cast(this->batch_ids.data().get());
+				auto d_needed_next = thrust::device_pointer_cast(this->needed_next.data().get());
+				auto d_needed_next_size = thrust::device_pointer_cast(this->needed_next_size.data().get());
 
 				thrust::fill(thrust::device, d_predecessors, d_predecessors + batch_size * num_vertices, -1);
+				thrust::fill(thrust::device, d_batches, d_batches + batch_size * num_vertices, 0);
+				thrust::fill(thrust::device, d_needed_next, d_needed_next + batch_size * num_vertices, -1);
+				thrust::fill(thrust::device, d_needed_next_size, d_needed_next_size + num_vertices, 0);
 				thrust::fill(thrust::device, d_distances, d_distances + batch_size * num_vertices,
 							 std::numeric_limits<vertex_t>::max());
 
 				for (int i = 0; i < batch_size; ++i) {
 					vertex_t s = this->param.sources[i];
+					d_needed_next[s * batch_size] = i;
+					d_needed_next_size[s] = 1;
+					d_batches[i * num_vertices + s] = 1;
 					d_predecessors[i * num_vertices + s] = s;
 					d_distances[i * num_vertices + s] = 0;
 				}
@@ -86,25 +103,40 @@ namespace gunrock {
 				auto batch_size = P->param.sources.size();
 				auto num_vertices = G.get_number_of_vertices();
 				auto iteration = this->iteration;
+				auto batches = P->batch_ids.data().get();
+				auto needed_next_size = P->needed_next_size.data().get();
+				auto needed_next = P->needed_next.data().get();
 
 				auto search = [=] __host__ __device__(
 									  vertex_t const &source,
 									  vertex_t const &neighbor,
 									  edge_t const &edge,
 									  weight_t const &weight) -> bool {
-					bool updated = false;
-					for (int batch = 0; batch < batch_size; ++batch) {
-						vertex_t *dist_row = distances + batch * num_vertices;
-						vertex_t *pred_row = predecessors + batch * num_vertices;
+					bool updated_smth = false;
+					for (int i = 0; i < needed_next_size[source]; ++i) {
+						auto batch = needed_next[batch_size * source + i];
+						if (batches[batch * num_vertices + source] == 1) {
+							auto idx = batch * num_vertices + neighbor;
+							if (gunrock::math::atomic::cas(&batches[idx], static_cast<vertex_t>(0), static_cast<vertex_t>(1)) == static_cast<vertex_t>(0)) {
+								auto old_size = needed_next_size[neighbor];
+								gunrock::math::atomic::add(&needed_next_size[neighbor], 1);
+								gunrock::math::atomic::cas(&needed_next[batch_size * neighbor + old_size], static_cast<vertex_t>(-1), batch);
 
-						vertex_t new_dist = dist_row[source] + 1;
-						vertex_t old_dist = gunrock::math::atomic::min(&dist_row[neighbor], new_dist);
-						if (new_dist < old_dist) {
-							gunrock::math::atomic::cas(&pred_row[neighbor], static_cast<vertex_t>(-1), source);
-							updated = true;
+								auto old_source_size = needed_next_size[source];
+								gunrock::math::atomic::add(&needed_next_size[source], -1);
+								gunrock::math::atomic::cas(&needed_next[batch_size * source + old_source_size], batch, static_cast<vertex_t>(-1));
+
+								vertex_t *dist_row = distances + batch * num_vertices;
+								vertex_t *pred_row = predecessors + batch * num_vertices;
+
+								dist_row[neighbor] = dist_row[source] + 1;
+								pred_row[neighbor] = source;
+								updated_smth = true;
+							}
 						}
 					}
-					return updated;
+
+					return updated_smth;
 				};
 
 				gunrock::operators::advance::execute<
